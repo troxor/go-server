150af9609cfd915f90e2378db453102e69b5d9a7
Troy C <troxor@users.noreply.github.com>
2015-10-29 23:51:24 -0500

    endless is great

--- go-server.go
+++ go-server.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 import "net/http"
+import "time"
 import "html"
 import "log"
+import "github.com/fvbock/endless"
 
 func main() {
+	log.Print("Starting net/http server on 8000")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2*time.Second)
 		fmt.Fprintf(w, "Hello %q\n", html.EscapeString(r.URL.Path))
 	})
-	err := http.ListenAndServe(":8000", nil)
+	err := endless.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
